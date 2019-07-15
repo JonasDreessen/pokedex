76c1be21e5785efe1534aa612143f80cb994be47
JonasDreessen <dreessenjonas95@gmail.com>
2019-07-15 11:05:24 +0200

    add new function for image

--- assets/main.js
+++ assets/main.js
@@ -13,11 +13,27 @@ pokedex();
 function displayPokemon(response){
     // declare html elements 
     let cardTitle = document.querySelector(".card-title");
-    let cardPic = document.querySelector(".card-picture");
     let cardInformation = document.querySelector(".card-information");
     // get information out of api
     let pokeName = response.data.name;
-    let pokePic = response.data.sprites.front_default;
-    console.log(pokePic);
-    console.log(response);
+    let pokeWeight = response.data.weight;
+    
+    // append api info to DOM
+    cardTitle.innerHTML= pokeName;
+    cardInformation.innerHTML = `Weight: ${pokeWeight}`;
+
+    createPokeImage(response);
+}
+
+function createPokeImage(response){
+    // declare html element
+    let cardPic = document.querySelector(".card-picture");
+    // get info out of api
+    let pokePicURL = response.data.sprites.front_default;
+    // create image 
+    let createPokePic = document.createElement("img");
+    createPokePic.src = pokePicURL;
+    // append api to DOM
+    cardPic.appendChild(createPokePic);
+
 }
